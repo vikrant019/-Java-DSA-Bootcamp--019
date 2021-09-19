2da1dc694c1476b37824094b040c8f2dbbf16332
Vikrant Dhakane <vikrantdhakane2017@gmail.com>
2021-09-19 10:21:41 +0530

    Q6_commissionPercentageCalculator.java

---
+++ Assignments/03-conditionals-loops/Intermediate Java Programs/Q6_commissionPercentageCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q6_commissionPercentageCalculator {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter sale price: ");
+		float salePrice=sc.nextFloat();
+		System.out.println("Enter comission %: ");
+		float cPer=sc.nextFloat();
+		System.out.println("comission : "  + (salePrice*cPer)/100);
+	}
+
+}
